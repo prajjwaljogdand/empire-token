7696374c590f7b96f78a81912b94faa5d3bfb1c2
prajjwaljogdand <prajjwaljogdand@gmail.com>
2021-10-06 10:45:50 +0530

    3d model updated

--- js/threed.js
+++ js/threed.js
@@ -12,35 +12,14 @@ renderer.gammaOutput = true;
 
 
 let tl = new gsap.timeline({ repeat: -1 });
+let tl2 = new gsap.timeline({ repeat: -1 });
+let tl3 = new gsap.timeline({ repeat: -1 });
 var loader = new THREE.GLTFLoader();
-
-
-
-loader.load('./logo2.glb', function (glb) {
-    console.log(glb)
-    root = glb.scene;
-    root.scale.set(0.16, 0.16, 0.2)
-    root.position.set(-5.39, 3.15, -0.2)
-    root.rotation.set(0.09, 0.05, 0)
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 5;
-    scene.add(root);
-    scene.rotation.set(0, 0.4, 0)
-    tl.to(scene.rotation, { x: 0, y: -0.4, duration: 3 });
-    tl.to(scene.rotation, { x: 0, y: 0.4, duration: 3 });
-  
-
-}, function (xhr) {
-    console.log((xhr.loaded / xhr.total * 100) + "% loaded")
-}, function (error) {
-    console.log(error)
-})
 var light = new THREE.AmbientLight(0x24DDDC, 0.001, 100)
 
 scene.add(light)
 
-var light_1 = new THREE.PointLight(0x24DDDC, 2, 10);
+var light_1 = new THREE.PointLight(0x24DDDC, 2, 100);
 light_1.position.set(1, 1, 0.5);
 light_1.castShadow = true;
 light_1.rotation.z = 50;
@@ -52,6 +31,31 @@ light_2.position.set(-1, -1, -0.3);
 light_2.castShadow = true;
 scene.add(light_2);
 light_2.rotation.z = 50;
+
+
+loader.load('./logo2.glb', function (glb) {
+    console.log(glb)
+    root = glb.scene;
+    root.scale.set(0.16, 0.16, 0.2)
+    root.position.set(-5.39, 3.15, -0.2)
+    
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 5;
+    scene.add(root);
+    scene.rotation.set(0, 0.7, 0)
+    tl.to(scene.rotation, { x: 0, y: -0.7, duration: 4, ease:Linear.easeInOut ,delay: 0.1  });
+    tl.to(scene.rotation, { x: 0, y: 0.7, duration: 4, ease:Linear.easeInOut , delay: 0.1});
+   tl2.to(light_2.position ,{z : 0.4 , duration : 4, delay: 0.1})
+   tl2.to(light_2.position ,{z : -0.4 , duration : 4, delay: 0.1})
+
+ 
+}, function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + "% loaded")
+}, function (error) {
+    console.log(error)
+})
+
 
 
 
